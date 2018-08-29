368a309740beed80d9198396244f561c11cfc33e
Vicsai <victsai1221@gmail.com>
2018-08-29 11:24:28 +0800

    added parseMessage and bot responds to basic commands

--- betterBeebo.js
+++ betterBeebo.js
@@ -1,7 +1,32 @@
-const Discord = require('discord.io');
+const Discord = require('discord.js');
 const auth = require('./auth.json');
 
-const bot = new Discord.Client({
-  token: auth.token,
-  autorun: true
+const games = [];
+
+const bot = new Discord.Client();
+
+function parseMessage(message) {
+  const formatted=message.substring(1);
+  const parsed = message.content.split(' ');
+  return parsed;
+}
+bot.login(auth.token);
+
+bot.on('ready', () => {
+  console.log('beebo lives!');
+});
+bot.on('message', message => {
+  if (!message.content.startsWith('!') || message.author.bot) return;
+  const parsed = parseMessage(message);
+  const command = parsed[0];
+  const arg= parsed[1];
+  if (command === 'test') message.channel.send('I AM BEEBO');
+  if (command === 'add') {
+    games.push(arg);
+    message.channel.send('successfully added');
+  }
+  if (command==='remove'){
+    if (games.find(arg)
+  }
+  if (command === 'show') message.channel.send(games.toString());
 });
