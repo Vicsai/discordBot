22a8806fcbc3cd6fc0a52b22b259644461650512
Vicsai <victsai1221@gmail.com>
2018-09-04 15:58:35 +0800

    refactor igl and fixed user join notification

--- betterBeebo.js
+++ betterBeebo.js
@@ -2,10 +2,12 @@
 const Discord = require('discord.js');
 const auth = require('./auth.json');
 // variables
+let server;
+let channels;
+let textChannel;
+
 const games = [];
 const commands = {};
-let voiceChannel;
-let textChannel = '';
 const ttsArray = [];
 
 const bot = new Discord.Client();
@@ -20,51 +22,33 @@ bot.login(auth.token);
 
 bot.on('ready', () => {
   console.log('beebo lives!');
+  server = bot.guilds.get(auth.serverID);
+  textChannel = server.channels.get(auth.serverID);
+  channels = Array.from(server.channels.keys());
   for (let i = 0; i < Object.keys(commands).length; i += 1) ttsArray.push(false);
 });
 bot.on('message', message => {
   if (!message.content.startsWith('!') || message.author.bot) return;
-  textChannel = message.channel;
-  const parsed = parseMessage(message);
-  const command = parsed[0];
-  const arg = parsed[1];
-  ({ voiceChannel } = message.member);
-
-  if (Object.keys(commands).indexOf(command) !== -1) {
-    const commandIndex = Object.keys(commands).indexOf(command);
+  // textChannel = message.channel;
+  const [command, arg] = parseMessage(message);
+  const commandIndex = Object.keys(commands).indexOf(command);
+  if (commandIndex !== -1) {
     const commandTTS = ttsArray[commandIndex];
     const mes = commands[command](arg);
     if (mes !== '-1') textChannel.send(mes, { tts: commandTTS });
   }
 });
 bot.on('voiceStateUpdate', (oldMember, newMember) => {
-  // use newMember.voiceChannelID to detect which channel
   // TODO change to not use tts
   const oldUserChannel = oldMember.voiceChannel;
   const newUserChannel = newMember.voiceChannel;
-  const oldUsers = [];
-  const newUsers = [];
-  // check if voice channel is not empty before/after change
-  if (newUserChannel !== undefined && oldUserChannel !== undefined) {
-    // record all users in new state
-    for (const [, guildMember] of newUserChannel.members) {
-      newUsers.push(guildMember.user.username);
-    }
-    // record all users in old state
-    for (const [, guildMember] of oldUserChannel.members) {
-      oldUsers.push(guildMember.user.username);
-    }
-    // check if there are more users after the state change
-    if (newUsers.length > oldUsers.length) {
-      for (let i = 0; i < newUsers.length; i += 1) {
-        if (oldUsers.indexOf(newUsers[i]) === -1) {
-          textChannel.send(`${newUsers[i]} has joined your channel`, { tts: true });
-        }
-      }
-    }
+  // check if user joined a channel
+  if (newUserChannel !== undefined && oldUserChannel === undefined) {
+    textChannel.send(`${newMember.user.username} has joined your channel`);
   }
 });
 // COMMANDS
+
 // adding/deleting and picking a game
 commands.add = function add(game) {
   if (game === undefined) return 'please provide a game to add';
@@ -90,14 +74,21 @@ commands.pick = function pick() {
 };
 // picking igl
 commands.igl = function igl() {
-  if (voiceChannel === undefined) return 'please join a voice channel';
-  const usersInChannel = voiceChannel.members;
-  const users = [];
-  for (const [, guildMember] of usersInChannel) {
-    users.push(guildMember.user.username);
+  for (let i = 0; i < channels.length; i += 1) {
+    const currentChannel = server.channels.get(channels[i]);
+    const members = Array.from(currentChannel.members.keys());
+    if (currentChannel.type === 'voice' && members.length === 1)
+      return 'not enough people in channel';
+    if (currentChannel.type === 'voice' && members.length > 1) {
+      const users = [];
+      for (const [, guildMember] of currentChannel.members) {
+        users.push(guildMember.user.username);
+      }
+      const rand = Math.floor(Math.random() * users.length);
+      return `${users[rand]} is team leader`;
+    }
   }
-  const rand = Math.floor(Math.random() * users.length);
-  return `${users[rand]} is team leader`;
+  return 'no one in voice channel';
 };
 // toggle tts on commands
 commands.tts = function tts(command) {
