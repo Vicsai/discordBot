f7d79cde69734f4bc7197d6154212d859e12ad0d
Vicsai <victsai1221@gmail.com>
2018-09-26 12:32:29 -0700

    fixed formatdate,changed how messages are sent

--- betterBeebo.js
+++ betterBeebo.js
@@ -21,20 +21,26 @@ function parseMessage(message) {
   return parsed;
 }
 function formatDate(date) {
-  const formattedDate = `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}`;
-  console.log(formattedDate);
+  const month = `0${date.getMonth() + 1}`;
+  const formattedDate = `${date.getFullYear()}-${month.substring(0, 2)}-${date.getDate()}`;
   return formattedDate;
+}
+// TODO add tts
+function sendMessage(message) {
+  textChannel.send(message);
+  return message;
 }
 
 bot.login(auth.token);
 
 bot.on('ready', () => {
   console.log('beebo lives!');
-  server = bot.guilds.get('484192628586577934'); // 484192628586577934
+  server = bot.guilds.get(auth.serverID); // 484192628586577934
   channels = Array.from(server.channels.keys());
   for (let i = 0; i < channels.length; i += 1) {
     if (server.channels.get(channels[i]).type === 'text') {
       textChannel = server.channels.get(channels[i]);
+      return;
     }
   }
   for (let i = 0; i < Object.keys(commands).length; i += 1) ttsArray.push(false);
@@ -44,9 +50,7 @@ bot.on('message', message => {
   const [command, arg] = parseMessage(message);
   const commandIndex = Object.keys(commands).indexOf(command);
   if (commandIndex !== -1) {
-    const commandTTS = ttsArray[commandIndex];
-    const mes = commands[command](arg);
-    if (mes !== '-1') textChannel.send(mes, { tts: commandTTS });
+    commands[command](arg);
   }
 });
 bot.on('voiceStateUpdate', (oldMember, newMember) => {
@@ -55,79 +59,102 @@ bot.on('voiceStateUpdate', (oldMember, newMember) => {
   const newUserChannel = newMember.voiceChannel;
   // check if user joined a channel
   if (newUserChannel !== undefined && oldUserChannel === undefined) {
-    textChannel.send(`${newMember.user.username} has joined your channel`);
+    sendMessage(`${newMember.user.username} has joined your channel`);
   }
 });
 // COMMANDS
 
 // add game into games array
 commands.add = function add(game) {
-  if (game === undefined) return 'please provide a game to add';
+  if (game === undefined) {
+    sendMessage('please provide a game to add');
+    return 'undefined (add game)';
+  }
   games.push(game);
-  return `successfully added ${game}`;
+  sendMessage(`successfully added ${game}`);
+  return 'success (add game)';
 };
 // remove game from games array if found
 commands.remove = function remove(game) {
-  if (game === undefined) return 'please provide a game to remove';
+  if (game === undefined) {
+    sendMessage('please provide a game to remove');
+    return 'undefined (remove game)';
+  }
   const index = games.indexOf(game);
   if (index !== -1) {
     games.splice(index, 1);
-    return `successfully removed ${game}`;
+    sendMessage(`successfully removed ${game}`);
+    return 'success (remove game)';
   }
-  return `${game} was not in the array`;
+  sendMessage(`${game} was not in the array`);
+  return 'game not in array (remove game)';
 };
 // lists values in array games
 commands.show = function show() {
-  if (games.length !== 0) return games.toString();
-  return 'no games in array';
+  if (games.length !== 0) {
+    sendMessage(games.toString());
+    return 'success (show game)';
+  }
+  sendMessage('no games in array');
+  return 'no games in array (show game)';
 };
 commands.pick = function pick() {
   const rand = Math.floor(Math.random() * games.length);
-  return games[rand];
+  sendMessage(games[rand]);
+  return 'success (pick game)';
 };
 // randomly selects a user from a voice channel
 commands.igl = function igl() {
   for (let i = 0; i < channels.length; i += 1) {
     const currentChannel = server.channels.get(channels[i]);
     const members = Array.from(currentChannel.members.keys());
-    if (currentChannel.type === 'voice' && members.length === 1)
-      return 'not enough people in channel';
+    if (currentChannel.type === 'voice' && members.length === 1) {
+      sendMessage('not enough people in channel');
+      return 'not enough people (igl)';
+    }
     if (currentChannel.type === 'voice' && members.length > 1) {
       const users = [];
       for (let j = 0; j < members.length; j += 1) {
         users.push(currentChannel.members.get(members[j]));
       }
       const rand = Math.floor(Math.random() * users.length);
-      return `${users[rand]} is team leader`;
+      sendMessage(`${users[rand]} is team leader`);
+      return 'success (igl)';
     }
   }
-  return 'no one in voice channel';
+  sendMessage('no one in voice channel');
+  return 'no one in channel (igl)';
 };
 commands.tvAdd = function tvAdd(tvSeries) {
   tvShows.push(tvSeries);
-  return `${tvSeries} is added`;
+  sendMessage(`${tvSeries} is added`);
+  return 'success (add show)';
 };
 commands.tvGuide = function tvGuide(x) {
   // x can pass in day of the week or all; if nothing then defaults to today
-  const date = new Date();
+  let date = new Date();
   if (x === undefined) {
-    formatDate(date);
+    date = formatDate(date);
   }
   tvmaze.schedule('US', date, (error, response) => {
-    console.log(JSON.parse(response));
-
-    return '0';
+    const sched = JSON.parse(response);
+    sched.forEach(episode => {
+      if (tvShows.indexOf(episode.show.name) !== -1) {
+        const mes = `${episode.show.name} ${episode.season}x${episode.number} ${episode.name}`;
+        sendMessage(mes);
+      }
+    });
   });
 };
 // toggle tts on commands
-commands.tts = function tts(command) {
-  const commandIndex = Object.keys(commands).indexOf(command);
-  if (commandIndex !== -1) {
-    ttsArray[commandIndex] = !ttsArray[commandIndex];
-    return `tts for ${command} is ${ttsArray[commandIndex]}`;
-  }
-  return `unable to toggle tts for ${command}`;
-};
+// commands.tts = function tts(command) {
+//   const commandIndex = Object.keys(commands).indexOf(command);
+//   if (commandIndex !== -1) {
+//     ttsArray[commandIndex] = !ttsArray[commandIndex];
+//     return `tts for ${command} is ${ttsArray[commandIndex]}`;
+//   }
+//   return `unable to toggle tts for ${command}`;
+// };
 // help
 commands.help = function help() {
   return Object.keys(commands);
